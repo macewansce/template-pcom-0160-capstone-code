b88050ddb256c624b33ba0f53293eea7abcfc590
Dean Smith <smithd343@macewan.ca>
2025-07-18 11:35:48 -0600

    Remove references to weatherforecast

--- capstone.web.api/Program.cs
+++ capstone.web.api/Program.cs
@@ -119,26 +119,6 @@ namespace capstone.web.api
 
             app.MapUserEndpoints();
 
-            //var summaries = new[]
-            //{
-            //    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-            //};
-
-            //app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            //{
-            //    var forecast = Enumerable.Range(1, 5).Select(index =>
-            //        new WeatherForecast
-            //        {
-            //            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-            //            TemperatureC = Random.Shared.Next(-20, 55),
-            //            Summary = summaries[Random.Shared.Next(summaries.Length)]
-            //        })
-            //        .ToArray();
-            //    return forecast;
-            //})
-            //.WithName("GetWeatherForecast")
-            //.WithOpenApi();
-
             app.Run();
         }
         static void SeedDatabase(AppDbContext context)
