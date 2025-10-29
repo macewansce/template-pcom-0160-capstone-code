9cbccd726ad1f9ddce38f7d80639b280a7d84c53
Dean Smith <smithd343@macewan.ca>
2025-10-29 11:56:17 -0600

    Update initial migration script

--- capstone.web.api/Migrations/20250101010101_CreateInitialSchema.cs
+++ capstone.web.api/Migrations/20250101010101_CreateInitialSchema.cs
@@ -16,43 +16,18 @@ namespace capstone.web.api.Migrations
         {
             migrationBuilder.Sql
             (
-                $"IF ( OBJECT_ID('Users') IS NOT NULL ) BEGIN PRINT 'Users Table Exists' END ELSE BEGIN CREATE TABLE Users ( Id int NOT NULL IDENTITY, FirstName nvarchar(max) NOT NULL, LastName nvarchar(max) NOT NULL, Email nvarchar(max) NOT NULL, Username nvarchar(max) NOT NULL, PasswordHash nvarchar(max) NOT NULL, Role nvarchar(max) NOT NULL, CONSTRAINT PK_Users PRIMARY KEY (Id) ) END"
+                $"IF OBJECT_ID(N'[Users]') IS NOT NULL BEGIN PRINT 'Table [Users] Exists' END ELSE BEGIN CREATE TABLE [Users] ( [Id] int NOT NULL IDENTITY, [FirstName] nvarchar(max) NOT NULL, [LastName] nvarchar(max) NOT NULL, [Email] nvarchar(max) NOT NULL, [Username] nvarchar(max) NOT NULL, [PasswordHash] nvarchar(max) NOT NULL, [Role] nvarchar(max) NOT NULL, CONSTRAINT [PK_Users] PRIMARY KEY ([Id]) ) END;"
             );
-            //migrationBuilder.CreateTable
-            //(
-            //    name: "Users",
-            //    columns: table => new
-            //    {
-            //        Id = table.Column<int>(type: "int", nullable: false)
-            //            .Annotation("SqlServer:Identity", "1, 1"),
-            //        FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-            //        LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
-            //        Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
-            //        Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
-            //        PasswordHash = table.Column<string>(type: "nvarchar(max)", nullable: false),
-            //        Role = table.Column<string>(type: "nvarchar(max)", nullable: false)
-            //    },
-            //    constraints: table =>
-            //    {
-            //        table.PrimaryKey("PK_Users", x => x.Id);
-            //    }
-            //);
             string apassword = BCrypt.Net.BCrypt.HashPassword("admin-password");
+            migrationBuilder.Sql
+            (
+                $"IF EXISTS (SELECT [Username] from [Users] where Username='admin') BEGIN PRINT 'User [admin] exists' END ELSE BEGIN INSERT INTO [Users] ([FirstName], [LastName], [Email], [Username], [PasswordHash], [Role]) VALUES ('Admin', 'User', 'admin@example.com', 'admin', '{apassword}', 'Administrator') END;" 
+            );
             string gpassword = BCrypt.Net.BCrypt.HashPassword("general-password");
             migrationBuilder.Sql
             (
-                $"IF NOT EXISTS (SELECT Username from Users where Username='admin') BEGIN INSERT INTO Users (FirstName, LastName, Email, Username, PasswordHash, Role) VALUES ('Admin', 'User', 'admin@example.com', 'admin', '{apassword}', 'Administrator') END IF NOT EXISTS (SELECT Username from Users where Username='general') BEGIN INSERT INTO Users (FirstName, LastName, Email, Username, PasswordHash, Role) VALUES ('General', 'User', 'general@example.com', 'general', '{gpassword}', 'General') END;"
+                $"IF EXISTS (SELECT [Username] from [Users] where Username='general') BEGIN PRINT 'User [general] exists' END ELSE BEGIN INSERT INTO [Users] ([FirstName], [LastName], [Email], [Username], [PasswordHash], [Role]) VALUES ('General', 'User', 'general@example.com', 'general', '{gpassword}', 'General') END;"
             );
-            //migrationBuilder.InsertData
-            //(
-            //    table: "Users",
-            //    columns: new[] { "FirstName", "LastName", "Email", "Username", "PasswordHash", "Role" },
-            //    values: new object[,]
-            //    {
-            //        { "Admin", "User", "admin@example.com", "admin",  BCrypt.Net.BCrypt.HashPassword("admin-password"), "Administrator" },
-            //        { "General", "User", "general@example.com", "general",  BCrypt.Net.BCrypt.HashPassword("general-password"), "General" }
-            //    }
-            //);
         }
 
         /// <inheritdoc />0
